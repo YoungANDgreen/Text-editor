275bc2a7d31b1c4a928e1c547ca1355e011d83b1
Ryan Younggreen <ryounggreen@gmail.com>
2023-02-14 18:13:07 -0700

    successful run

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const dbPut = async (content) => {
+  console.log('PUT success');
+  const dbJate = await openDB('jate', 1);
+  const txt = dbJate.transaction('jate', 'readwrite');
+  const store = txt.objectStore('jate');
+  const request = store.put({ id: 1, jate: content });
+  const result = await request;
+  console.log('Saved succesfully to database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const dbGet = async () => {
+  console.log('GET success');
+  const dbJate = await openDB('jate', 1);
+  const txt = dbJate.transaction('jate', 'readonly');
+  const store = txt.objectStore('jate');
+  const request = store.get(1);
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
